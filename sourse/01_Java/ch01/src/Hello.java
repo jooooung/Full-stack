c27066789a415353e3c35762dfcb1802bb4924ff
jooooung <dkswndrnjs12@gmail.com>
2022-11-28 17:37:15 +0900

    java_ch01

---
+++ sourse/01_Java/ch01/src/Hello.java
@@ -0,0 +1,8 @@
+
+public class Hello {
+	// 자바 파일명은 클래스 명과 동일, 대문자로 시작, 클래스 명에는 영문자, 숫자, _
+	//JVM이 실행하는 main함수
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+}
