94f1c0102ad415e2b1685e3e1ab6179f7062f3e3
jooooung <dkswndrnjs12@gmail.com>
2022-12-12 11:41:06 +0900

    11

--- sourse/01_Java/ch13_pattern/src/singletone1/Single.java
+++ sourse/01_Java/ch13_pattern/src/singletone1/Single.java
@@ -1,5 +1,15 @@
 package singletone1;
 
 public class Single {
+	private int i;
+	//持失切
+	//五社球
 
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
 }
--- sourse/01_Java/ch13_pattern/src/singletone1/SingleMain.java
+++ sourse/01_Java/ch13_pattern/src/singletone1/SingleMain.java
@@ -5,6 +5,8 @@ public class SingleMain {
 	public static void main(String[] args) {
 		Single obj1 = new Single();
 		Single obj2 = new Single();
-		
+		obj1.setI(99);
+		System.out.println(obj1.getI());
+		System.out.println(obj2.getI());
 	}
 }
