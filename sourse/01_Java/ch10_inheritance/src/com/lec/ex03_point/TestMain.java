1a04d972a40f0519ceca38e58522be92ead7abc5
jooooung <dkswndrnjs12@gmail.com>
2022-12-22 00:20:43 +0900

    18

--- sourse/01_Java/ch10_inheritance/src/com/lec/ex03_point/TestMain.java
+++ sourse/01_Java/ch10_inheritance/src/com/lec/ex03_point/TestMain.java
@@ -5,11 +5,10 @@ public class TestMain {
 	public static void main(String[] args) {
 		Point point1 = new Point();
 		Point point2 = new Point(1, 2);
-		System.out.println(point1.infoPrint());		
+		System.out.println(point1.infoPrint());
 		System.out.println(point2.infoPrint());
 		System.out.println("======================");
 		Point3D point3d = new Point3D(10, 20, 30);
-//		point3d.infoPrint();  추후에 변경(아직 x y 값만 나옴)
 		System.out.println(point3d.infoPrint());
 	}
 }
