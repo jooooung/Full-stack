474853e8bcada32262fe9f2f6f5054d3cd53a991
jooooung <dkswndrnjs12@gmail.com>
2023-03-05 21:48:30 +0900

    64_JSP-EL, forward 2

--- sourse/01_Java/ch04_controlStatement/src/com/lec/ConditionQuiz/Quiz5.java
+++ sourse/01_Java/ch04_controlStatement/src/com/lec/ConditionQuiz/Quiz5.java
@@ -9,13 +9,13 @@ public class Quiz5 {
 		int month = sc.nextInt();
 		switch(month){
 		case 12: case 1: case 2:
-			System.out.println("입력하신 개월은 겨울이네요");break;
+			System.out.println(month + "월은 겨울이네요");break;
 		case 3: case 4: case 5:
-			System.out.println("입력하신 개월은 봄이네요");break;
+			System.out.println(month + "월은 봄이네요");break;
 		case 6: case 7: case 8:
-			System.out.println("입력하신 개월은 여름이네요");break;
+			System.out.println(month + "월은 여름이네요");break;
 		case 9: case 10: case 11:
-			System.out.println("입력하신 개월은 가을이네요");break;
+			System.out.println(month + "월은 가을이네요");break;
 		default:
 			System.out.println("유효한 개월 수가 아닙니다");
 		}
